0dec2fc32c45fb1b4194bf10d9ab458364ac3015
Kirll Valyavin <kvalyavin@yandex.ru>
2022-08-07 16:24:27 +0300

    avoid testing dependencies

--- default.nix
+++ default.nix
@@ -8,22 +8,32 @@ let
 
   inherit (pkgs.lib.attrsets) mapAttrs;
   
-  haskellPackages = pkgs.haskell.packages.${versions.haskellCompiler};
-  
-  myHaskellPackages = haskellPackages.override {
-    overrides = self: super: with pkgs.haskell.lib;
-      let new =
-            mapAttrs (_: value:
-              dontCheck (self.callHackageDirect value {})
-            ) versions.haskellOverrides;
-      in new // {
-        stylish-haskell = overrideCabal
-          new.stylish-haskell ( old: {
-            configureFlags = [ "-f +ghc-lib" ];
-          });
-        trial-chain = self.callCabal2nix "trial-chain" ./. {};
-      };
+  haskellPackages = pkgs.haskell.packages.${versions.haskellCompiler}.override {
+    overrides = self: super:
+      mapAttrs (_: v:
+        if    builtins.typeOf v == "set"
+           && builtins.hasAttr "isHaskellLibrary" v
+           && v.isHaskellLibrary
+        then pkgs.haskell.lib.dontCheck v
+        else v) super;
   };
+  
+  myHaskellPackages = haskellPackages.override (old: {
+    overrides = pkgs.lib.composeExtensions old.overrides (self: super: with pkgs.haskell.lib;
+      let
+        new =
+          mapAttrs (_: value:
+            dontCheck (self.callHackageDirect value {})
+          ) versions.haskellOverrides;
+      in
+        new // {
+          stylish-haskell = overrideCabal
+            new.stylish-haskell ( old: {
+              configureFlags = [ "-f +ghc-lib" ];
+            });
+          trial-chain = self.callCabal2nix "trial-chain" ./. {};
+        });
+  });
     
   projectShell = myHaskellPackages.shellFor {
     packages = p:
